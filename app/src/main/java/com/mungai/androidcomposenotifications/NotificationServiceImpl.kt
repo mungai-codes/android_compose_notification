f37ed0fe73d34561a804e71a79d31bdb46c3efcb
mungai-codes <koolmungai@gmail.com>
2023-05-22 21:16:34 +0300

    Final commit

--- app/src/main/java/com/mungai/androidcomposenotifications/NotificationServiceImpl.kt
+++ app/src/main/java/com/mungai/androidcomposenotifications/NotificationServiceImpl.kt
@@ -27,8 +27,14 @@ class NotificationServiceImpl @Inject constructor(
         manager.notify(
             1, builder
                 .setSmallIcon(R.drawable.old_man)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE)
                 .setContentTitle("Updated version")
                 .setContentText("This is an updated notification.")
+                .setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText("This is the big text, its much longer that it can't fit on one line.")
+                        .setBigContentTitle("Big content title; ps Its soooo Bigggg!!")
+                )
                 .build()
         )
     }
